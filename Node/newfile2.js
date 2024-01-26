9690c05511b06eda4bb3b223eb080ff019d11df1
lokeshkharkwal7 <lokeshkharkwal@gmail.com>
2024-01-27 02:00:11 +0530

    Day 16 progress added

--- Node/newfile2.js
+++
@@ -1,9 +0,0 @@
-
-
-// Import the 'http' module
-// const http = require('http');
-// http.createServer((req,res)=>{
-//     res.write("Hello this is the firse java script program")
-//     res.end()
-// }).listen(4500)
-
--- Node/tempCodeRunnerFile.js
+++ Node/tempCodeRunnerFile.js
@@ -1,7 +1,2 @@
-console.log("Node is Installed")
-// Import the 'http' module
-const http = require('http');
-http.createServer((req,res)=>{
-    res.write("Hello this is the firse java script program")
-    res.end()
-}).listen(4500)
+
+// fs.unlinkSync()
